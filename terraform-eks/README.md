4c95c1b092916380fe08cc832c298320ede6cb13
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2023-05-31 22:30:32 -0400

    add terraform-eks video code

---
+++ terraform-eks/README.md
@@ -0,0 +1,8 @@
+# Terraform EKS
+
+This code accompanies the terraform eks video
+
+## Files
+
+`complete-example.tf` - full complete example with VPC and EKS cluster
+`starter-example.tf` - DIY starter example with VPC (add your own EKS code)
---
+++ terraform-eks/starter-example.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "ascode-cluster"
+  region = "us-east-1"
+
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["us-east-1a", "us-east-1b"]
+
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+
+  tags = {
+    Example = local.name
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
